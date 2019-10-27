6fba9011eaf846c263be2a7164f59c82b4ca78f5
sarahkel <G00364736@gmit.ie>
2019-10-27 21:56:36 +0000

    added read stock from csv

--- shop.c
+++ shop.c
@@ -1,5 +1,10 @@
+// GMIT Data Analytics 
+// Adapted from: Multi-paradigm programming - Dr. Dominic Carr
+
+
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 struct Product {
     char* name;
@@ -15,30 +20,69 @@ struct Customer {
     char* name;
     double budget;
     struct ProductStock shoppingList[10];
+    int index;
 };
 
 struct Shop {
     double cash;
     struct ProductStock stock[20];
+    int index;
 };
 
 void printProduct(struct Product p)
 {
-    printf("----------------\n");
     printf("PRODUCT NAME: %s \nPRODUCT PRICE: %.2f\n", p.name, p.price);   
     printf("----------------\n");
 };
 
+void printCustomer(struct Customer c)
+{
+    printf("CUSTOMER NAME: %s \nCUSTOMER BUDGET: %.2f\n", c.name, c.budget);   
+    printf("----------------\n");
+    for(int i = 0; i < c.index; i++)
+    {
+        printProduct(c.shoppingList[i].product);
+        printf("%s ORDERS %d of ABOVE PRODUCT\n", c.name, c.shoppingList[i].quantity);
+        double cost = c.shoppingList[i].quantity * c.shoppingList[i].product.price;
+        printf("The cost to %s will be E%.2f\n", c.name, cost);
+        printf("----------------\n");
+    }
+};
+
+void createAndStockShop()
+{
+    struct Shop shop = { 200 };
+    FILE* fp;
+    char* line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    fp = fopen("stock.csv", "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+
+    while ((read = getline(&line, &len, fp)) != -1) {
+        // printf("Retrieved line of length %zu:\n", read);
+        printf("%s IS A LINE\n", line); 
+    }
+}
+
 int main(void)
 {
-    struct Customer sarah = { "Sarah", 100.0 };
-    printf("Customer name is %s\n",sarah.name);
+    // struct Customer sarah = { "Sarah", 100.0 };
     
-    struct Product coke = { "Can Coke", 1.10 };
-    printProduct(coke);
+    // struct Product coke = { "Can Coke", 1.10 };
+    // struct Product bread = { "Bread", 0.7 };
+    // // printProduct(coke);
 
-    struct ProductStock cokeStock = { coke, 20};
-    printf("The shop has %d of the product %s\n", cokeStock.quantity, cokeStock.product.name);
+    // struct ProductStock cokeStock = { coke, 20};
+    // struct ProductStock breadStock = { bread, 2};
+    // sarah.shoppingList[sarah.index++] = cokeStock;
+    // sarah.shoppingList[sarah.index++] = breadStock;
+    // printCustomer(sarah);
+    // printf("The shop has %d of the product %s\n", cokeStock.quantity, cokeStock.product.name);
+
+    createAndStockShop();
 
     return 0;
 }
